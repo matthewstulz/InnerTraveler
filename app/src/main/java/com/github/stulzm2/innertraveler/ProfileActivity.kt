8f02ae6df61141605443ca1168f1a974a7ee3986
matthewstulz <stulzm2@nku.edu>
2018-03-11 13:58:12 -0400

    Can choose profile image and displays first name

--- app/src/main/java/com/github/stulzm2/innertraveler/ProfileActivity.kt
+++ app/src/main/java/com/github/stulzm2/innertraveler/ProfileActivity.kt
@@ -1,24 +1,30 @@
 package com.github.stulzm2.innertraveler
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : BaseActivity() {
 
-    private var mDatabase: DatabaseReference? = null
+    private var uri: Uri? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var mDatabaseReference: DatabaseReference? = null
     private var mAuth: FirebaseAuth? = null
     private var mAuthListener: FirebaseAuth.AuthStateListener? = null
     private var numberOfPosts: Int = 0
     private var currentFirebaseUser: FirebaseUser? = null
+    private var storage: StorageReference? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +32,9 @@ class ProfileActivity : BaseActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Profile"
 
-        mDatabase = FirebaseDatabase.getInstance().reference
+        storage = FirebaseStorage.getInstance().reference
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
         mAuthListener = FirebaseAuth.AuthStateListener {
             if (mAuth!!.currentUser == null) {
                 val loginIntent = Intent(this@ProfileActivity, LoginActivity::class.java)
@@ -46,6 +54,52 @@ class ProfileActivity : BaseActivity() {
 
             override fun onCancelled(databaseError: DatabaseError) {}
         })
+
+        mDatabaseReference!!.child(currentFirebaseUser!!.uid)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        tvProfileName.text = dataSnapshot.child("firstName")?.value as String?
+                        val profileImage = dataSnapshot.child("profilePhoto")?.value as String?
+                        Glide.with(this@ProfileActivity).load(profileImage).into(ivProfile)
+
+                    }
+                    override fun onCancelled(databaseError: DatabaseError) {}
+                })
+
+        ivProfile!!.setOnClickListener {
+            val galleryIntent = Intent(Intent.ACTION_GET_CONTENT)
+            galleryIntent.type = "image/*"
+            startActivityForResult(galleryIntent, ProfileActivity.GALLERY_REQUEST_CODE)
+        }
+    }
+
+    private fun updateProfileImage() {
+        val filepath = storage!!.child("users_profile_images").child(uri!!.lastPathSegment)
+        filepath.putFile(uri!!).addOnSuccessListener { taskSnapshot ->
+            val downloadUrl = taskSnapshot.downloadUrl
+            Toast.makeText(applicationContext, "Profile Image update", Toast.LENGTH_SHORT).show()
+            val updateCurrentUser = mDatabaseReference!!.child(currentFirebaseUser!!.uid)
+            updateCurrentUser.child("profilePhoto").setValue(downloadUrl!!.toString()).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this@ProfileActivity, ProfileActivity::class.java))
+                }
+            }
+        }
+    }
+
+    override// image from gallery result
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            uri = data.data
+            Glide.with(this).load(uri).into(ivProfile)
+            updateProfileImage()
+        }
+    }
+
+    companion object {
+        private val GALLERY_REQUEST_CODE = 2
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
